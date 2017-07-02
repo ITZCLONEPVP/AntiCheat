1d93b4014ec85c7df3ef3cac2893d0b442081823
ImKyd <kralikmatej@seznam.cz>
2017-07-02 08:27:29 +0200

    Update Main.php

--- src/SuperAntiCheat/Main.php
+++ src/SuperAntiCheat/Main.php
@@ -1,1 +1,15 @@
+<?php
+
+namespace SuperAntiCheat;
+
+use pocketmine\plugin\PluginBase;
+
+class SuperAntiCheat extends PluginBase{
+  
+  public function onEnable(){
+    $this->getLogger()->info("Anticheat try to protect your server from hackers!");
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+  }
+}
+
 
